c1edd8b4106350a37d958efe36f097b4e22a62c1
olehedza <BiewZpo2>
2020-08-18 19:09:46 +0300

    create User entity

---
+++ src/main/java/com/olehedza/simplespringblog/model/User.java
@@ -0,0 +1,22 @@
+package com.olehedza.simplespringblog.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String password;
+    private String email;
+}
