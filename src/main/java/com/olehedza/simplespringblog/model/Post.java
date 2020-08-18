16d2218a65dd4eb3e5ee7d65f3ca884d08777427
olehedza <BiewZpo2>
2020-08-18 19:03:25 +0300

    create Post entity

---
+++ src/main/java/com/olehedza/simplespringblog/model/Post.java
@@ -0,0 +1,32 @@
+package com.olehedza.simplespringblog.model;
+
+import java.time.Instant;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String title;
+    @Lob
+    @NotEmpty
+    private String text;
+    private Instant createdOn;
+    private Instant updatedOn;
+    @NotBlank
+    private String username;
+}
