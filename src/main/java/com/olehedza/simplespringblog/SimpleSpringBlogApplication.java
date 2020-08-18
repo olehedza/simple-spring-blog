e7eae619876e10ab643b0a23a5d31523d89cecb5
olehedza <BiewZpo2>
2020-08-18 17:21:56 +0300

    initial project state

---
+++ src/main/java/com/olehedza/simplespringblog/SimpleSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.olehedza.simplespringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBlogApplication.class, args);
+	}
+
+}
